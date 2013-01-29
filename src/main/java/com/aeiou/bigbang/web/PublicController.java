66b1471043cdda66812c54fa001eeca9ba98973a
tjiang <casproteus@hotmail.com>
2013-01-29 09:53:05 -0500

    added a publiContent view-controller, and setted the index page forward to it.

--- src/main/java/com/aeiou/bigbang/web/PublicController.java
+++ src/main/java/com/aeiou/bigbang/web/PublicController.java
@@ -3,10 +3,14 @@ package com.aeiou.bigbang.web;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.aeiou.bigbang.domain.Content;
 
 @RequestMapping("/public/**")
 @Controller
@@ -14,11 +18,20 @@ public class PublicController {
 
     @RequestMapping(method = RequestMethod.POST, value = "{id}")
     public void post(@PathVariable Long id, ModelMap modelMap, HttpServletRequest request, HttpServletResponse response) {
-    System.out.println("go to his big uncle's!");
+    	System.out.println("go to his big uncle's!");
     }
 
-    @RequestMapping
-    public String index() {
+    @RequestMapping(produces = "text/html")
+    public String index(@RequestParam(value = "page", required = false) Integer page, @RequestParam(value = "size", required = false) Integer size, Model uiModel){
+    	if (page != null || size != null) {
+            int sizeNo = size == null ? 10 : size.intValue();
+            final int firstResult = page == null ? 0 : (page.intValue() - 1) * sizeNo;
+            uiModel.addAttribute("contents", Content.findContentEntries(firstResult, sizeNo));
+            float nrOfPages = (float) Content.countContents() / sizeNo;
+            uiModel.addAttribute("maxPages", (int) ((nrOfPages > (int) nrOfPages || nrOfPages == 0.0) ? nrOfPages + 1 : nrOfPages));
+        } else {
+            uiModel.addAttribute("contents", Content.findAllContents());
+        }
         return "public/index";
     }
 }

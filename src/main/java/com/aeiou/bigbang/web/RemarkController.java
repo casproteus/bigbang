80167dbabea9b29d0f06d9d2b95a874f68cc1b23
tjiang <casproteus@hotmail.com>
2013-04-15 15:12:50 -0400

    removed the Remark to from list_detail_twitter.

--- src/main/java/com/aeiou/bigbang/web/RemarkController.java
+++ src/main/java/com/aeiou/bigbang/web/RemarkController.java
@@ -15,11 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.aeiou.bigbang.domain.Content;
 import com.aeiou.bigbang.domain.Remark;
 import com.aeiou.bigbang.domain.Twitter;
 import com.aeiou.bigbang.domain.UserAccount;
 import com.aeiou.bigbang.services.secutiry.UserContextService;
+import com.aeiou.bigbang.util.BigAuthority;
 import com.aeiou.bigbang.util.SpringApplicationContext;
 
 @RequestMapping("/remarks")
@@ -29,9 +29,17 @@ public class RemarkController {
 
 	@Inject
 	private UserContextService userContextService;
+
+	void populateEditForm(Model uiModel, Remark remark) {
+        uiModel.addAttribute("remark", remark);
+        addDateTimeFormatPatterns(uiModel);
+        uiModel.addAttribute("twitters", Twitter.findAllTwitters());
+        uiModel.addAttribute("useraccounts", UserAccount.findAllUserAccounts());
+        uiModel.addAttribute("authorities",BigAuthority.getRemarkOptions());
+    }
 	
 	@RequestMapping(method = RequestMethod.POST, produces = "text/html")
-    public String create(@Valid Remark remark, BindingResult bindingResult, Twitter pTwitter, Model uiModel, HttpServletRequest httpServletRequest) {
+    public String create(@Valid Remark remark, BindingResult bindingResult, @RequestParam(value = "pTwitterId", required = false)Long pTwitterId, Model uiModel, HttpServletRequest httpServletRequest) {
 		//TODO: Should make the check before submit.
 		if(remark.getContent() == null || remark.getContent().length() < 1)
             return "remarks/create";
@@ -53,7 +61,7 @@ public class RemarkController {
 		if (bindingResult.hasErrors()) {
 			if (bindingResult.getAllErrors().size() == 1 && remark.getPublisher() == null) {
 				remark.setPublisher(tUserAccount);
-				
+				remark.setRemarkto(Twitter.findTwitter(pTwitterId));
 				remark.setRemarkTime(new Date());//add remark time when it's submitted.
 			} else {
 				populateEditForm(uiModel, remark);
@@ -67,29 +75,6 @@ public class RemarkController {
         
         PublicController tController = SpringApplicationContext.getApplicationContext().getBean("publicController", PublicController.class);
         return tController.showDetailTwitters(remark.getRemarkto().getId(), null, null, uiModel);
-        /*
-        int sizeNo = 25;
-        final int firstResult = 0;
-        
-        UserAccount tOwner = remark.getRemarkto().getPublisher();
-
-    	UserAccount tCurUser = tCurName == null ? null : UserAccount.findUserAccountByName(tCurName);
-    	Set<Integer> tAuthSet = BigAuthority.getAuthSet(tCurUser, tOwner);
-        uiModel.addAttribute("spaceOwner", tOwner);
-        float nrOfPages;
-        Twitter tTwitter = remark.getRemarkto();
-        uiModel.addAttribute("twitter", tTwitter);
-        uiModel.addAttribute("remarks", Remark.findRemarkByTwitter(tTwitter, tAuthSet, firstResult, sizeNo));
-        nrOfPages = (float) Remark.countRemarksByTwitter(tTwitter, tAuthSet) / sizeNo;
-    	uiModel.addAttribute("maxPages", (int) ((nrOfPages > (int) nrOfPages || nrOfPages == 0.0) ? nrOfPages + 1 : nrOfPages));
-    	Remark tRemark = new Remark();
-        uiModel.addAttribute("newremark", tRemark);
-        uiModel.addAttribute("authorities",BigAuthority.getRemarkOptions());
-        List<Twitter> remarktos = new ArrayList<Twitter>();
-        remarktos.add(tTwitter);
-        uiModel.addAttribute("remarktos", remarktos);
-        
-        return "public/list_detail_twitter";*/
     }
 	
 	@RequestMapping(method = RequestMethod.PUT, produces = "text/html")

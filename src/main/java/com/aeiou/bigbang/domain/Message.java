0c732577e44c9f35b76d3bad65a9ac46eada0fd4
tjiang <casproteus@hotmail.com>
2013-04-23 22:21:20 -0400

    added a new table for message, so messages don't appear together with normal remarks.

---
+++ src/main/java/com/aeiou/bigbang/domain/Message.java
@@ -0,0 +1,35 @@
+package com.aeiou.bigbang.domain;
+
+import java.util.Date;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
+import org.springframework.roo.addon.tostring.RooToString;
+
+@RooJavaBean
+@RooToString
+@RooJpaActiveRecord
+public class Message {
+
+    @NotNull
+    @ManyToOne
+    private UserAccount receiver;
+
+    @NotNull
+    @ManyToOne
+    private UserAccount sender;
+
+    @NotNull
+    private String content;
+
+    @NotNull
+    @Temporal(TemporalType.TIMESTAMP)
+    @DateTimeFormat(style = "M-")
+    private Date postTime;
+
+    private int status;
+}

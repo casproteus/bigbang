7ca0955902f231b12e4a9b47f5688a67f09bee6e
tjiang <casproteus@hotmail.com>
2013-08-18 09:26:35 -0400

    fixed the problem of Chinese user name mess.

--- src/main/java/com/aeiou/bigbang/util/MyCookieThemeResolver.java
+++ src/main/java/com/aeiou/bigbang/util/MyCookieThemeResolver.java
@@ -1,5 +1,7 @@
 package com.aeiou.bigbang.util;
 
+import java.net.URLEncoder;
+
 import javax.inject.Inject;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -18,17 +20,28 @@ public class MyCookieThemeResolver extends CookieThemeResolver{
 		if (themeName != null) {
 			// Set request attribute and add cookie.
 			request.setAttribute(THEME_REQUEST_ATTRIBUTE_NAME, themeName);
-//			addCookie(response, themeName);
-			//todo:logged in and is in his personal space
+			//addCookie(response, themeName);
+			
+			//if logged in user changes theme in his personal space, we modify only his personal space theme.
+			//by saving the theme into account info.
 			String tURLPath = request.getServletPath();
 			String tCurName = userContextService.getCurrentUserName();
-			if(tURLPath != null && tCurName != null && tURLPath.equals("/" + tCurName)){
-				UserAccount tUserAccount = UserAccount.findUserAccountByName(tCurName);
-				tCurName = tUserAccount.getName();
-				tUserAccount.setTheme(Integer.parseInt(themeName));
-				tUserAccount.persist();				//save the theme into account info.
+			if(tURLPath != null && tCurName != null){
+				if(!tURLPath.equals("/" + tCurName))
+					try{
+						tURLPath = new String(tURLPath.getBytes("ISO-8859-1"), "UTF-8");
+					}catch(Exception e){
+					}
+				
+				if(tURLPath.equals("/" + tCurName)){
+					UserAccount tUserAccount = UserAccount.findUserAccountByName(tCurName);
+					tCurName = tUserAccount.getName();
+					tUserAccount.setTheme(Integer.parseInt(themeName));
+					tUserAccount.persist();
+				}
 			}
-			//save the theme into cookie
+			// save the theme into cookie anyway, so if user changed the them in his own space, 
+			//we bet he'd like to see the public space with this theme.
 			final Cookie cookie = new Cookie("theme", themeName);
 			cookie.setMaxAge(31536000); // One year
 			cookie.setPath("/");

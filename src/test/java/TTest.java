b671149457a73826d632ec29beb5199a8405db58
tjiang <casproteus@hotmail.com>
2013-08-23 13:36:27 -0400

    added selenium test which can perform locally. modified the head1 picture.

---
+++ src/test/java/TTest.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+
+import org.joda.time.TimeOfDay;
+
+
+public class TTest {
+	public static void main(String[] args){
+		TimeOfDay a = new TimeOfDay();
+		TimeOfDay b = new TimeOfDay();
+		System.out.println(a);
+		System.out.println(b);
+	}
+}

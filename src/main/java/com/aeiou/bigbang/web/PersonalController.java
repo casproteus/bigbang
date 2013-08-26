edf38ba9c78b647e167af5a2036b15b19c0828d0
tjiang <casproteus@hotmail.com>
2013-08-25 22:35:00 -0400

    make the two blog areas have same height. so it looks better.

--- src/main/java/com/aeiou/bigbang/web/PersonalController.java
+++ src/main/java/com/aeiou/bigbang/web/PersonalController.java
@@ -226,7 +226,7 @@ public class PersonalController{
     	//so, can not use the tauthset directly. the logic should be:
     	//if current user is owner, then display public and visible to friend ones, otherwise, display only public ones
     	tAuthSet = BigAuthority.getAuthSetForTwitterOfFriends(tCurUser, tOwner);
-    	List<Twitter> twitterRight = Twitter.findTwitterByOwner(tOwner, tAuthSet, 0, 8, null);
+    	List<Twitter> twitterRight = Twitter.findTwitterByOwner(tOwner, tAuthSet, 0, twitterLeft == null ? 8 : twitterLeft.size(), null);
         uiModel.addAttribute("twitterLeft", twitterLeft);
         uiModel.addAttribute("twitterRight", twitterRight);
         //---------------------------------------------------------------------------------

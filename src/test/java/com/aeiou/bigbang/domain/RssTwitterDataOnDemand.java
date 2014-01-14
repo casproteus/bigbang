ad9e879f13003a7aed31f202a363cd6325c543d2
tjiang <casproteus@hotmail.com>
2014-01-13 23:07:24 -0500

    added a new table with two fields used Roo.

---
+++ src/test/java/com/aeiou/bigbang/domain/RssTwitterDataOnDemand.java
@@ -0,0 +1,7 @@
+package com.aeiou.bigbang.domain;
+
+import org.springframework.roo.addon.dod.RooDataOnDemand;
+
+@RooDataOnDemand(entity = RssTwitter.class)
+public class RssTwitterDataOnDemand {
+}

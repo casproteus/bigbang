b2b185f03b244ab1a78360f971c08c9d45097112
tao <casproteus@hotmail.com>
2017-09-29 13:49:34 -0400

    code formatting

--- src/main/java/com/aeiou/bigbang/services/synchronization/ClientSyncTool.java
+++ src/main/java/com/aeiou/bigbang/services/synchronization/ClientSyncTool.java
@@ -15,56 +15,51 @@ import flexjson.JSONDeserializer;
 
 public class ClientSyncTool {
 
-    SynchnizationManager synchnizationManager = new SynchnizationManager();
+	SynchnizationManager synchnizationManager = new SynchnizationManager();
 
-    public int testLog(
-            UserAccount tCurUser,
-            String pCommand) {
-        ClientConfig config = new DefaultClientConfig();
-        Client client = Client.create(config); // if we need a ssh, can go on work in this part.
+	public int testLog(UserAccount tCurUser, String pCommand) {
+		ClientConfig config = new DefaultClientConfig();
+		Client client = Client.create(config); // if we need a ssh, can go on work in this part.
 
-        WebResource webResource = client.resource("http://localhost/bigbang/useraccounts/loglog");
-        webResource.accept("application/json"); // other methods in controller.
+		WebResource webResource = client.resource("http://localhost/bigbang/useraccounts/loglog");
+		webResource.accept("application/json"); // other methods in controller.
 
-        JsonObject json = new JsonObject();
-        json.addProperty("tag", "message");
-        ClientResponse response = webResource.type("application/json").post(ClientResponse.class, json.toString());
-        if (200 == response.getStatus()) {
-            List<String> tList =
-                    new JSONDeserializer<List<String>>().use(null, ArrayList.class).use("values", String.class)
-                            .deserialize((String) response.getEntity(String.class));
-        }
-        return -1;
-    }
+		JsonObject json = new JsonObject();
+		json.addProperty("tag", "message");
+		ClientResponse response = webResource.type("application/json").post(ClientResponse.class, json.toString());
+		if (200 == response.getStatus()) {
+			List<String> tList = new JSONDeserializer<List<String>>().use(null, ArrayList.class)
+					.use("values", String.class).deserialize(response.getEntity(String.class));
+		}
+		return -1;
+	}
 
-    public int startToSynch(
-            UserAccount tCurUser,
-            String pCommand) {
+	public int startToSynch(UserAccount tCurUser, String pCommand) {
 
-        ClientConfig config = new DefaultClientConfig();
-        // SSLContext ctx = SSLContext.getInstance("SSL");
-        // ctx.init(null, myTrustManager, null);
-        // config.getProperties().put(HTTPSProperties.PROPERTY_HTTPS_PROPERTIES, new HTTPSProperties(hostnameVerifier,
-        // ctx));
-        Client client = Client.create(config); // if we need a ssh, can go on work in this part.
+		ClientConfig config = new DefaultClientConfig();
+		// SSLContext ctx = SSLContext.getInstance("SSL");
+		// ctx.init(null, myTrustManager, null);
+		// config.getProperties().put(HTTPSProperties.PROPERTY_HTTPS_PROPERTIES, new
+		// HTTPSProperties(hostnameVerifier,
+		// ctx));
+		Client client = Client.create(config); // if we need a ssh, can go on work in this part.
 
-        // have to add an value: /{pCommand}, to distinguish
-        WebResource webResource = client.resource("http://teamup.sharethegoodones.com/useraccounts/" + pCommand);
-        webResource.accept("application/json"); // other methods in controller.
-        // webResource.method("POST"); //this method will call onto the remote server, duplicated with
-        // webResource.post();
+		// have to add an value: /{pCommand}, to distinguish
+		WebResource webResource = client.resource("http://teamup.sharethegoodones.com/useraccounts/" + pCommand);
+		webResource.accept("application/json"); // other methods in controller.
+		// webResource.method("POST"); //this method will call onto the remote server,
+		// duplicated with
+		// webResource.post();
 
-        ClientResponse response =
-                webResource.type("application/json").post(ClientResponse.class,
-                        synchnizationManager.getRecentlyAddedContent(tCurUser.getName(), pCommand));
-        // ClientResponse response = webResource.post(ClientResponse.class);
-        if (200 == response.getStatus()) {
-            List<String> tList =
-                    new JSONDeserializer<List<String>>().use(null, ArrayList.class).use("values", String.class)
-                            .deserialize((String) response.getEntity(String.class));
-            return synchnizationManager.saveContentIntoLocalDB(tList, pCommand);
-        }
-        return -1;
-    }
+		ClientResponse response = webResource.type("application/json").post(ClientResponse.class,
+				synchnizationManager.getRecentlyAddedContent(tCurUser.getName(), pCommand));
+		// ClientResponse response = webResource.post(ClientResponse.class);
+		if (200 == response.getStatus()) {
+			List<String> tList = new JSONDeserializer<List<String>>().use(null, ArrayList.class)
+					.use("values", String.class).deserialize(response.getEntity(String.class));
+			return synchnizationManager.saveContentIntoLocalDB(tList, pCommand);
+		}
+		return -1;
+	}
 
 }

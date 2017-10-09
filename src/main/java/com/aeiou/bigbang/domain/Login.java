303e509a4540f175b96e690db95d7f1d93b77987
tao <casproteus@hotmail.com>
2017-10-08 20:04:13 -0400

    Because of postgresQL not supportted well by heidi(sql client I choosed to use), and can not login into the postgres on aws, so I have to change to MySQl :(

--- src/main/java/com/aeiou/bigbang/domain/Login.java
+++ src/main/java/com/aeiou/bigbang/domain/Login.java
@@ -1,24 +1,28 @@
 package com.aeiou.bigbang.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 import org.springframework.beans.factory.annotation.Configurable;
 
 /**
- * this class looks like is used by no other class, while you can not delete it, because it's use through aspect. don't
- * know by who.
+ * this class looks like is used by no other class, while you can not delete it,
+ * because it's use through aspect. don't know by who.
  */
 @Entity
 @Configurable
 public class Login {
-    private boolean rememberMe;
 
-    public boolean getRememberMe() {
-        return rememberMe;
-    }
+	@Id
+	private Long id;
 
-    public void setRememberMe(
-            boolean rememberMe) {
-        this.rememberMe = rememberMe;
-    }
+	private boolean rememberMe;
+
+	public boolean getRememberMe() {
+		return rememberMe;
+	}
+
+	public void setRememberMe(boolean rememberMe) {
+		this.rememberMe = rememberMe;
+	}
 }

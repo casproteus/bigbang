ae6b1bb76916c153d3de77b8822416cf00b6984b
tao <casproteus@hotmail.com>
2017-10-10 15:07:55 -0400

    fixed issue that tag name displayed as null null null null. (don't know why the converter was added, while the convert method was never called? is it a bug of sprong roo new version?)

--- src/main/java/com/aeiou/bigbang/web/ApplicationConversionServiceFactoryBean.java
+++ src/main/java/com/aeiou/bigbang/web/ApplicationConversionServiceFactoryBean.java
@@ -16,4 +16,32 @@ public class ApplicationConversionServiceFactoryBean extends FormattingConversio
     // super.installFormatters(registry);
     // // Register application converters and formatters
     // }
+
+    public void installLabelConverters(
+            FormatterRegistry registry) {
+        // registry.addConverter(getBigTagToStringConverter());
+        registry.addConverter(getIdToBigTagConverter());
+        registry.addConverter(getStringToBigTagConverter());
+        registry.addConverter(getCircleToStringConverter());
+        registry.addConverter(getIdToCircleConverter());
+        registry.addConverter(getStringToCircleConverter());
+        registry.addConverter(getContentToStringConverter());
+        registry.addConverter(getIdToContentConverter());
+        registry.addConverter(getStringToContentConverter());
+        registry.addConverter(getCustomizeToStringConverter());
+        registry.addConverter(getIdToCustomizeConverter());
+        registry.addConverter(getStringToCustomizeConverter());
+        registry.addConverter(getMessageToStringConverter());
+        registry.addConverter(getIdToMessageConverter());
+        registry.addConverter(getStringToMessageConverter());
+        registry.addConverter(getRemarkToStringConverter());
+        registry.addConverter(getIdToRemarkConverter());
+        registry.addConverter(getStringToRemarkConverter());
+        registry.addConverter(getTwitterToStringConverter());
+        registry.addConverter(getIdToTwitterConverter());
+        registry.addConverter(getStringToTwitterConverter());
+        registry.addConverter(getUserAccountToStringConverter());
+        registry.addConverter(getIdToUserAccountConverter());
+        registry.addConverter(getStringToUserAccountConverter());
+    }
 }
